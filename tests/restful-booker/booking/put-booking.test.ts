ccee5b4bba66ca94782372879fef445e0ac4452b
Jhanina Romero <jhanina.romero9@gmail.com>
2026-08-13 23:31:26 -0400

    Fix update method and add auth method to sent token

--- tests/restful-booker/booking/put-booking.test.ts
+++ tests/restful-booker/booking/put-booking.test.ts
@@ -1,9 +1,19 @@
-import {BookingClient } from '../../../src/restful-booker/clients/BookingClient';
+import { AuthClient } from '../../../src/restful-booker/clients/AuthClient';
+import { BookingClient } from '../../../src/restful-booker/clients/BookingClient';
 
 describe('PUT /booking/:id', () => {
     const bookingClient = new BookingClient();
 
     test('should update an existing booking', async () => {
+        const authClient = new AuthClient();
+
+        const authResponse = await authClient.createToken({
+            username: 'admin',
+            password: 'password123'
+        });
+
+        const token = authResponse.data.token;
+
         const updatedBooking = {
             firstname: 'Jane',
             lastname: 'Smith',
@@ -17,7 +27,7 @@ describe('PUT /booking/:id', () => {
         };
         const bookingId = 1;
 
-        const response = await bookingClient.updateBooking(bookingId, updatedBooking);
+        const response = await bookingClient.updateBooking(bookingId, updatedBooking, token);
 
         expect(response.status).toBe(200);
         expect(response.data).toEqual(updatedBooking);
