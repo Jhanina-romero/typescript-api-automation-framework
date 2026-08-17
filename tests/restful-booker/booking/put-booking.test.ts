e22c737787c8b8f74f9af21c6ac859f5b470b766
Jhanina Romero <jhanina.romero9@gmail.com>
2026-08-17 18:16:38 -0400

    remove coments

--- tests/restful-booker/booking/put-booking.test.ts
+++ tests/restful-booker/booking/put-booking.test.ts
@@ -2,38 +2,6 @@ import { AuthClient } from '../../../src/restful-booker/clients/AuthClient';
 import { BookingClient } from '../../../src/restful-booker/clients/BookingClient';
 import { BookingFactory } from '../../../src/factories/booking.factory';
 
-// describe('PUT /booking/:id', () => {
-//     const bookingClient = new BookingClient();
-
-//     test('should update an existing booking', async () => {
-//         const authClient = new AuthClient();
-
-//         const authResponse = await authClient.createToken({
-//             username: 'admin',
-//             password: 'password123'
-//         });
-
-//         const token = authResponse.data.token;
-
-//         const updatedBooking = {
-//             firstname: 'Jane',
-//             lastname: 'Smith',
-//             totalprice: 200,
-//             depositpaid: false,
-//             bookingdates: {
-//                 checkin: '2023-02-01',
-//                 checkout: '2023-02-10'
-//             },
-//             additionalneeds: 'Lunch'
-//         };
-//         const bookingId = 1;
-
-//         const response = await bookingClient.updateBooking(bookingId, updatedBooking, token);
-
-//         expect(response.status).toBe(200);
-//         expect(response.data).toEqual(updatedBooking);
-//     });
-// });
 describe('PUT /booking/:id', () => {
     const bookingClient = new BookingClient();
     let bookingId: number;
@@ -66,17 +34,6 @@ describe('PUT /booking/:id', () => {
     });
 
     it('should update an existing booking', async () => {
-
-        
-        // const authClient = new AuthClient();
-
-        // const authResponse = await authClient.createToken({
-        //     username: 'admin',
-        //     password: 'password123'
-        // });
-
-        // const token = authResponse.data.token;
-
         const updatedBooking = {
             firstname: 'Jane',
             lastname: 'Smith',
@@ -88,7 +45,6 @@ describe('PUT /booking/:id', () => {
             },
             additionalneeds: 'Lunch'
         };
-        //const bookingId = 1;
 
         const response = await bookingClient.updateBooking(bookingId, updatedBooking, token);
 
