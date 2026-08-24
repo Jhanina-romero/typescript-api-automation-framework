24c2ffd1a8d49a5a841eb79850d6ae3fc61838c8
Jhanina Romero <jhanina.romero9@gmail.com>
2026-08-24 19:16:54 -0400

    Configure and install allure for reports

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,12 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+
+  testEnvironment: 'allure-jest/node',
+
+  testEnvironmentOptions: {
+    resultsDir: 'allure-results'
+  },
 
   roots: [
     '<rootDir>/tests'
